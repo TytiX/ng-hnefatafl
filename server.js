d69fe7e5cbf081839acf8e747fd2f1b50c7bae32
ci-build <ci-build@klukas.net>
2019-09-12 08:55:41 +0000

    [skip ci] Updates

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use('/', express.static('ng-hnefatafl'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
